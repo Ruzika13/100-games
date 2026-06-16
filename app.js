8e5b3213f038428d984b701669b81351bacb4cf6
Ruzika13 <astramin15@outlook.com>
2026-06-16 21:21:27 +0300

    Refactor game connection handling for online play

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const App = {
     peer: null,
     conn: null,
-    currentGame: null, // Track active game for cleanup
+    currentGame: null,
+    activeOnlineGame: null, // 'ttt' or 'c4'
 
     showView: function(viewId) {
         // 1. Cleanup any running game before switching views
         if (this.currentGame === 'tictactoe') TicTacToe.cleanup();
         if (this.currentGame === 'barricade') Barricade.cleanup();
+        if (this.currentGame === 'connect4') Connect4.cleanup();
         this.currentGame = null;
 
         // 2. Switch views
@@ -20,6 +22,9 @@ const App = {
         } else if (viewId === 'game-barricade') {
             this.currentGame = 'barricade';
             Barricade.init();
+        } else if (viewId === 'game-c4') {
+            this.currentGame = 'connect4';
+            Connect4.init();
         }
     },
 
@@ -27,56 +32,66 @@ const App = {
         if (this.peer) return;
         this.peer = new Peer(); 
         this.peer.on('error', (err) => {
-            document.getElementById('online-status').innerText = "Connection error: " + err.type;
+            const statusEl = this.activeOnlineGame === 'c4' ? document.getElementById('c4-online-status') : document.getElementById('online-status');
+            if(statusEl) statusEl.innerText = "Connection error: " + err.type;
         });
     },
 
-    hostGame: function() {
+    hostGame: function(gameType = 'ttt') {
+        this.activeOnlineGame = gameType;
         this.initPeer();
         const code = Math.random().toString(36).substring(2, 8).toUpperCase();
         this.peer.destroy();
         this.peer = new Peer(code);
+        
+        const prefix = gameType === 'c4' ? 'c4-' : '';
         this.peer.on('open', (id) => {
-            document.getElementById('room-code-display').innerText = id;
-            document.getElementById('online-status').innerText = "Waiting for opponent to join...";
+            document.getElementById(`${prefix}room-code-display`).innerText = id;
+            document.getElementById(`${prefix}online-status`).innerText = "Waiting for opponent to join...";
         });
         this.peer.on('connection', (c) => {
             this.conn = c;
-            this.setupConnection();
-            document.getElementById('online-status').innerText = "Opponent joined! You are X. Starting...";
-            TicTacToe.start('online-host');
+            this.setupConnection(gameType);
+            document.getElementById(`${prefix}online-status`).innerText = "Opponent joined! You are Red (1st). Starting...";
+            if (gameType === 'c4') Connect4.start('online-host');
+            else TicTacToe.start('online-host');
         });
     },
 
-    joinGame: function() {
-        const code = document.getElementById('join-code-input').value.toUpperCase();
+    joinGame: function(gameType = 'ttt') {
+        this.activeOnlineGame = gameType;
+        const prefix = gameType === 'c4' ? 'c4-' : '';
+        const code = document.getElementById(`${prefix}join-code-input`).value.toUpperCase();
         if (code.length !== 6) {
             alert("Please enter a valid 6-character code.");
             return;
         }
         this.initPeer();
-        document.getElementById('online-status').innerText = "Connecting...";
+        document.getElementById(`${prefix}online-status`).innerText = "Connecting...";
         this.conn = this.peer.connect(code);
-        this.setupConnection();
+        this.setupConnection(gameType);
         this.conn.on('open', () => {
-            document.getElementById('online-status').innerText = "Connected! You are O. Waiting for host...";
-            TicTacToe.start('online-join');
+            document.getElementById(`${prefix}online-status`).innerText = "Connected! You are Yellow (2nd). Waiting for host...";
+            if (gameType === 'c4') Connect4.start('online-join');
+            else TicTacToe.start('online-join');
         });
     },
 
-    setupConnection: function() {
+    setupConnection: function(gameType) {
         this.conn.on('data', (data) => {
             if (data.type === 'move') {
-                TicTacToe.makeMove(data.index, data.player, false);
+                if (gameType === 'c4') Connect4.makeMove(data.col, data.player, false);
+                else TicTacToe.makeMove(data.index, data.player, false);
             } else if (data.type === 'restart') {
-                TicTacToe.resetBoard();
+                if (gameType === 'c4') Connect4.resetBoard();
+                else TicTacToe.resetBoard();
             }
         });
     },
 
-    broadcastMove: function(index, player) {
+    broadcastMove: function(payload) {
         if (this.conn && this.conn.open) {
-            this.conn.send({ type: 'move', index: index, player: player });
+            this.conn.send(payload);
         }
     },
 
